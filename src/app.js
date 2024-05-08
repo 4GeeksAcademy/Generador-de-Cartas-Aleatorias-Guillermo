240c9d980654ada4f85d3689f96671459dc76bd4
Guillermo <guillermo.gil.cor@gmail.com>
2024-05-08 20:45:42 +0100

    Creamos la carta y le asignamos los valores aleatorios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const cardNumberGenerator = () => {
+  let randomNum = Math.floor(Math.random() * 12) + 1;
+  let figures = "";
+  let num = document.querySelector("#cardNumber");
+  if (randomNum == 11) {
+    figures = "J";
+  } else if (randomNum == 12) {
+    figures = "Q";
+  } else if (randomNum == 13) {
+    figures = "K";
+  }
+  randomNum >= 11 ? (num.innerHTML = figures) : (num.innerHTML = randomNum);
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suitsNumberGenerator = () => {
+  let suitNum = Math.floor(Math.random() * 3);
+  let suitSimbol = "";
+  if (suitNum == 0) {
+    suitSimbol = "♦";
+  } else if (suitNum == 1) {
+    suitSimbol = "♥";
+  } else if (suitNum == 2) {
+    suitSimbol = "♠";
+  } else {
+    suitSimbol = "♣";
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = document.querySelectorAll(".card-header, .card-footer");
+
+  [...suit].forEach((tag) => {
+    tag.innerHTML = suitSimbol;
+  });
 };

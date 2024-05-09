cf1a03c16dabb1c8e4d8bba70032bb48bd9388fa
Guillermo <guillermo.gil.cor@gmail.com>
2024-05-09 12:46:48 +0100

    creamos rama con variable de JS por copilot

--- src/app.js
+++ src/app.js
@@ -1,59 +1,29 @@
 let num = document.querySelector("#cardNumber");
-
-const cardNumberGenerator = () => {
-  let randomNum = Math.floor(Math.random() * 12) + 1;
-  let figures = "";
-
-  if (randomNum == 11) {
-    figures = "J";
-  } else if (randomNum == 12) {
-    figures = "Q";
-  } else if (randomNum == 13) {
-    figures = "K";
-  }
-  randomNum >= 11 ? (num.innerHTML = figures) : (num.innerHTML = randomNum);
-};
-
-//let suit = document.querySelectorAll(".card-header, .card-footer");
 let suitTop = document.querySelector(".card-header");
 let suitBot = document.querySelector(".card-footer");
 
-const suitsNumberGenerator = () => {
-  let suitNum = Math.floor(Math.random() * 3);
-  let suitSimbol = "";
-  if (suitNum == 0) {
-    suitSimbol = "♦";
-    num.classList.add("red");
-    suitTop.classList.add("red");
-    suitBot.classList.add("red");
-  } else if (suitNum == 1) {
-    suitSimbol = "♥";
-    num.classList.add("red");
-    suitTop.classList.add("red");
-    suitBot.classList.add("red");
+const cardNumberGenerator = () => {
+  let randomNum = Math.floor(Math.random() * 12) + 1;
+  num.innerHTML = randomNum >= 11 ? ["J", "Q", "K"][randomNum - 11] : randomNum;
+};
 
-    num.classList.add("red");
-  } else if (suitNum == 2) {
-    suitSimbol = "♠";
-    num.classList.remove("red");
-    suitTop.classList.remove("red");
-    suitBot.classList.remove("red");
-  } else {
-    suitSimbol = "♣";
-    num.classList.remove("red");
-    suitTop.classList.remove("red");
-    suitBot.classList.remove("red");
-  }
+const suitsNumberGenerator = () => {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let colors = ["red", "red", "", ""];
+  let suitNum = Math.floor(Math.random() * 4);
+  let suitSimbol = suits[suitNum];
+  let color = colors[suitNum];
+
+  [num, suitTop, suitBot].forEach(el => {
+    el.classList.remove("red");
+    if (color) el.classList.add(color);
+  });
 
   suitTop.innerHTML = suitSimbol;
   suitBot.innerHTML = suitSimbol;
-
-  // [...suit].forEach((tag) => {
-  //   tag.innerHTML = suitSimbol;
-  // });
 };
 
-const init = () => {
+const init = () =>{
   suitsNumberGenerator();
   cardNumberGenerator();
 };

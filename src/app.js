8ad82701eadca0d751a51135da63b31e0ab3908f
Guillermo <guillermo.gil.cor@gmail.com>
2024-05-09 11:21:35 +0100

    cambiamos variablesde top y bot, añadimos clase red a diamantes y corazones

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+let num = document.querySelector("#cardNumber");
+
 const cardNumberGenerator = () => {
   let randomNum = Math.floor(Math.random() * 12) + 1;
   let figures = "";
-  let num = document.querySelector("#cardNumber");
+
   if (randomNum == 11) {
     figures = "J";
   } else if (randomNum == 12) {
@@ -12,22 +14,37 @@ const cardNumberGenerator = () => {
   randomNum >= 11 ? (num.innerHTML = figures) : (num.innerHTML = randomNum);
 };
 
+//let suit = document.querySelectorAll(".card-header, .card-footer");
+let suitTop = document.querySelector(".card-header");
+let suitBot = document.querySelector(".card-footer");
+
 const suitsNumberGenerator = () => {
   let suitNum = Math.floor(Math.random() * 3);
   let suitSimbol = "";
   if (suitNum == 0) {
     suitSimbol = "♦";
+    num.classList.add("red");
+    suitTop.classList.add("red");
+    suitBot.classList.add("red");
+
+
   } else if (suitNum == 1) {
     suitSimbol = "♥";
+    num.classList.add("red");
+    suitTop.classList.add("red");
+    suitBot.classList.add("red");
+
+    num.classList.add("red");
   } else if (suitNum == 2) {
     suitSimbol = "♠";
   } else {
     suitSimbol = "♣";
   }
 
-  let suit = document.querySelectorAll(".card-header, .card-footer");
+  suitTop.innerHTML = suitSimbol;
+  suitBot.innerHTML = suitSimbol;
 
-  [...suit].forEach((tag) => {
-    tag.innerHTML = suitSimbol;
-  });
+  // [...suit].forEach((tag) => {
+  //   tag.innerHTML = suitSimbol;
+  // });
 };
